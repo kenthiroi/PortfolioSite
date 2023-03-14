6c690f1125d9eb4fd69d5f1990987e1186868ace
Kent Hiroi <khiroi@mail.sfsu.edu>
2023-03-13 19:41:29 -0700

    first push

---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return <>
+    {/* Sidenav */}
+    <div id="page-contents">
+    </div>
+    <div id="page-background">
+      {/* Put edited video here */}
+      {/* <video src=></video> */}
+    </div>
+  </>
+};
+
+export default Home;
---
+++ src/components/sidenav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SideNav = () => {
+  return <div id="side-nav">
+    {/* Main, About Me, Projects (Code), Events (Esports), Resume */}
+  </div>
+}
+
+export default SideNav;
