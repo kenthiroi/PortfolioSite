68a7b655b4d26e08546b07711674cc6ea5a9ab4d
Kent Hiroi <khiroi@mail.sfsu.edu>
2023-03-21 23:55:05 -0700

    filling out projects page

--- src/pages/events_worked.jsx
+++ src/pages/events_worked.jsx
@@ -2,47 +2,70 @@ import React from "react";
 
 function EventsWorked(){
   return <div>
-    <h1>2018</h1>
+      <h1>2018</h1>
+      <div>
+        April 7th, NoodsNoodsNoods Oakland edition powered by Foodbeast at ESA Oakland. A national tournament for Super Smash Bros Melee/Wii U
+      </div>
+      <div>
+        - July 7th, Bay Area Brawl. A regional tournament for Tekken 7, Street Fighter V, Dragon Ball FighterZ and Blazblue Cross Tag Battle at ESA Oakland.
+      </div>
+      <div>
+        - July 7th, Capcom Pro Tour Online 2018 NA West 2. Directed a broadcast for an online top 8 bracket for Street Fighter V with LevelupLive.
+      </div>
+      <div>
+        - July 14th, HCT Oakland. A national Hearthstone major at ESA Oakland.
+      </div>
+      <div>
+        - October 13th, Shell Showdown presented by TMNT/Nickelodeon at ESA Oakland.
+        A regional tournament for Super Smash Bros. Wii U.
+      </div>
+      <div>
+        - November 10th, Calicup. An exhibition match between San Francisco Shock and Los Angeles Valiant for Overwatch event at ESA Oakland.
+      </div>
+      <div>
+        - December 22nd, Super Splat Bros powered by Progressive at ESA Oakland. A national tournament for Super Smash Bros. Ultimate.
+      </div>
+
+      <h1>2019</h1>
+      <div>
+        - January 31st, Pre-Genesis 6 tournament at ESA Oakland. A pre-tourney for the super major Genesis 6 with over 400 entrants in one day for Super Smash Bros Melee/Ultimate.
+      </div>
+      <div>
+        - March 9th, Saturday Fight Night in collaboration with LevelupLive and NBC Bay Area at ESA Oakland. A Street Fighter tournament that was streamed to leveluplive and later on in the night showcased 5 players from each region (Norcal vs Socal) for a team battle on live television at NBC 11 Bay Area.
+      </div>
+      <div>
+        - March 16th, the Ultimate League tournament at ESA Oakland. A regional Super Smash Bros Ultimate tournament showcasing talent from remote ESA X Walmart locations, with a $5,000 prize pool.
+      </div>
+
+    <h1>2020</h1>
     <div>
-      April 7th, NoodsNoodsNoods Oakland edition powered by Foodbeast at ESA Oakland. A national tournament for Super Smash Bros Melee/Wii U
+      - Ronin Rumble 
+      -Delivered consistent high quality broadcast for multiple online tournament series.
+      <div>
+        - Granblue Fantasy: Versus - Weekly tournament series broadcasts
+      </div>
+      <div>
+        - Samurai Shodown - Monthly tournament series broadcasts
+      </div>
+      <div>
+        - Matcherino Cup Street Fighter 2 Super Turbo Finals Restream
+      </div>
+      <div>
+        - One off series with Killer Instinct, Blazblue Cross Tag Battle, Fall Guys
+      </div>
     </div>
+    
     <div>
-      - July 7th, Bay Area Brawl. A regional tournament for Tekken 7, Street Fighter V, Dragon Ball FighterZ and Blazblue Cross Tag Battle at ESA Oakland.
-    </div>
-    <div>
-      - July 7th, Capcom Pro Tour Online 2018 NA West 2. Directed a broadcast for an online top 8 bracket for Street Fighter V with LevelupLive.
-    </div>
-    <div>
-      - July 14th, HCT Oakland. A national Hearthstone major at ESA Oakland.
-    </div>
-    <div>
-      - October 13th, Shell Showdown presented by TMNT/Nickelodeon at ESA Oakland.
-      A regional tournament for Super Smash Bros. Wii U.
-    </div>
-    <div>
-      - November 10th, Calicup. An exhibition match between San Francisco Shock and Los Angeles Valiant for Overwatch event at ESA Oakland.
-    </div>
-    <div>
-      - December 22nd, Super Splat Bros powered by Progressive at ESA Oakland. A national tournament for Super Smash Bros. Ultimate.
+      Esports Engine
     </div>
 
-    <h1>2019</h1>
+    <h1>2021</h1>
     <div>
-      - January 31st, Pre-Genesis 6 tournament at ESA Oakland. A pre-tourney for the super major Genesis 6 with over 400 entrants in one day for Super Smash Bros Melee/Ultimate.
+      - Ronin Rumble 
+      <div>
+        - One off series with Tekken 7, Killer Instinct, Blazblue Cross Tag Battle
+      </div>
     </div>
-    <div>
-      - March 9th, Saturday Fight Night in collaboration with LevelupLive and NBC Bay Area at ESA Oakland. A Street Fighter tournament that was streamed to leveluplive and later on in the night showcased 5 players from each region (Norcal vs Socal) for a team battle on live television at NBC 11 Bay Area.
-    </div>
-    <div>
-      - March 16th, the Ultimate League tournament at ESA Oakland. A regional Super Smash Bros Ultimate tournament showcasing talent from remote ESA X Walmart locations, with a $5,000 prize pool.
-    </div>
-
-  <h1>2020</h1>
-  <div>
-    - Ronin Rumble - GBVS Sunday weekly broadcast:
-    -Delivered consistent high quality broadcast to an online weekly tournament series.
-  </div>
-
   </div>
 }
 
